b595cd12a8090f07cf51204753c325cad9605f14
abdulloh19 <parij4320@gmail.com>
2021-04-18 22:40:04 +0500

    scooter-imtihon

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+var burger = document.querySelector(".hamburger");
+var nav = document.querySelector(".nav");
+
+burger.addEventListener("click", function() {
+    nav.classList.toggle('nav--active');
+    burger.classList.toggle("hamburger--active")
+});
+
+window.addEventListener ("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
