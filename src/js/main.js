8f6523e4c8302f068b353c191b2e1389c5be38cd
abdulloh19 <parij4320@gmail.com>
2021-04-20 02:02:13 +0500

    sa

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 var burger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
-var tepaga = document.querySelector(".faqs__wrapp")
+var tepaga = document.querySelector(".faqs__wrapp");
+var body = document.querySelector("body");
 
 burger.addEventListener("click", function() {
     nav.classList.toggle('nav--active');
     burger.classList.toggle("hamburger--active");
+    body.classList.toggle("body--hidden")
 });
 tepaga.addEventListener('click', function() {
     tepaga.classList.toggle("faqs__wrapp--active")
