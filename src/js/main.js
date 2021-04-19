58745f283219c234fafe9f27e65d971a78b7d967
abdulloh19 <parij4320@gmail.com>
2021-04-20 01:02:05 +0500

    imtihonga

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 var burger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
+var tepaga = document.querySelector(".faqs__wrapp")
 
 burger.addEventListener("click", function() {
     nav.classList.toggle('nav--active');
-    burger.classList.toggle("hamburger--active")
+    burger.classList.toggle("hamburger--active");
+});
+tepaga.addEventListener('click', function() {
+    tepaga.classList.toggle("faqs__wrapp--active")
 });
 
 window.addEventListener ("scroll", function(){
