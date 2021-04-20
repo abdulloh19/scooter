dccc835d151ec2728aa0747a13c4ffaa19779553
abdulloh19 <parij4320@gmail.com>
2021-04-20 18:42:44 +0500

    bugungi

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,12 @@
 var burger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
-var tepaga = document.querySelector(".faqs__wrapp")
+var tepaga = document.querySelector(".faqs__wrapp");
+var body = document.querySelector("body");
 
 burger.addEventListener("click", function() {
     nav.classList.toggle('nav--active');
     burger.classList.toggle("hamburger--active");
+    body.classList.toggle("body--hidden")
 });
 tepaga.addEventListener('click', function() {
     tepaga.classList.toggle("faqs__wrapp--active")
@@ -13,4 +15,4 @@ tepaga.addEventListener('click', function() {
 window.addEventListener ("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
